add1005d6de3d7786ebc39b27ef24bc38b26b7d7
booldook <booldook@naver.com>
2020-04-26 10:50:48 +0900

    app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+/* 서버구동 */
+app.listen(3000, function(){
+	console.log("http://127.0.0.1:3000");
+});
+
+/* 미들웨어 */
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+/* 라우터 */
+app.use("/", express.static(	path.join(__dirname, './public')	));
+
+app.get();
+app.post();
