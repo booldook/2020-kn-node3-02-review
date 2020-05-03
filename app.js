bcc03e4c02f200a2fbb7783632a2824a2f086dda
booldook <booldook@naver.com>
2020-05-03 09:53:01 +0900

    pug 공부중

--- app.js
+++ app.js
@@ -20,13 +20,29 @@ app.use(express.urlencoded({extended: false}));
 /* 라우터 */
 app.use("/", express.static(	path.join(__dirname, './public')	));
 
+const navis = [
+	{id: 1, title: "About", link:"/about"},
+	{id: 2, title: "Board", link:"/board"},
+	{id: 3, title: "Gallery", link:"/gallery"},
+];
+
 app.get("/", (req, res, next) => {
-	const navis = [
-		{id: 1, title: "About", link:"#"},
-		{id: 2, title: "Board", link:"#"},
-		{id: 3, title: "Gallery", link:"#"},
-	];
 	const title = "index.html"
 	const headTitle = "PUG페이지"
 	res.render("index.pug", {navis, title, headTitle});
+});
+app.get("/about", (req, res, next) => {
+	const title = "about.html"
+	const headTitle = "PUG페이지"
+	res.render("about.pug", {navis, title, headTitle});
+});
+app.get("/board", (req, res, next) => {
+	const title = "board.html"
+	const headTitle = "PUG페이지"
+	res.render("board.pug", {navis, title, headTitle});
+});
+app.get("/gallery", (req, res, next) => {
+	const title = "gallery.html"
+	const headTitle = "PUG페이지"
+	res.render("gallery.pug", {navis, title, headTitle});
 });
